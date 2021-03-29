20338bf5ae0aeffd6c01f5e26d5d135d67baf82d
Ryan McKelvey <ryanmckelvey98@gmail.com>
2021-03-29 23:13:14 +0100

    Refactoring the API Call methodology and Implementing pushedAt list collection
    By implementing gql we can more easily call from the GraphQL API using the client.execute command. This way we can use a for loop to recursively search through each repo collected earlier and see their pushedAt datetimes, we however only need the latest datetime and will look into implementation later on in development.

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 # A Flask app set up using Githubs GraphQL API
-import requests
 import sys
+#from gql_resolver import resolve
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 from flask import Flask as fl
 
 app = fl(__name__)
 
-
-github_auth = "Your Github PAT here"
+# github_auth = "Your Github PAT here"
 
 
 headers = {"Authorization": github_auth}
 
+#Gql setup
+trnsprt = RequestsHTTPTransport(
+    url="https://api.github.com/graphql", headers={"Authorization": github_auth},)
+
+client = Client(
+  transport=trnsprt
+)
 
 # Query to get a users list of repos
-query = """
-{
+viewer_query = gql("""
+query{
   viewer{
-    name
-    repositories(privacy: PUBLIC, first: 100, after: "CURSOR OF BOUNDARY REPO==") {
+    login
+    repositories(privacy: PUBLIC, first: 100, after: "BOUNDARY CURSOR") {
       edges {
         node {
           name
@@ -26,18 +34,22 @@ query = """
     }
   }
 }
-"""
+""")
 
+repo_query = gql("""
+query($repo:String!, $viewer:String!) { 
+  repository(name: $repo, owner: $viewer) { 
+    pushedAt
+  }
+}
+""")
 
 # Function to execute GraphQL queries
-def run_query(query):
-    request = requests.post('https://api.github.com/graphql',
-                            json={'query': query}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(
-            request.status_code, query))
+def resolve(query,**params):
+  if len(params) > 0:
+    return client.execute(query, variable_values=params)
+  else:
+    return client.execute(query)
 
 
 # Function to get a string list of repository names for querying
@@ -45,15 +57,33 @@ def extract_repos_from_list(Lst):
     simplified_list = []
     for node in Lst:
         simplified_list.append(node['node']['name'])
-    print(simplified_list, file=sys.stderr)
     return simplified_list
+
+
+def get_name_from_query(res):
+    print(res)
+    return res['viewer']['login']
+
+
+def check_repo(Lst, viewer):
+    dates_list = []
+    for repo in Lst:
+        params = {
+            "repo": repo,
+            "viewer": viewer
+        }
+        dates_list.append(resolve(repo_query, **params)['repository']['pushedAt'])
+    return dates_list
 
 
 @app.route('/')
 def get_repos():
-    result = run_query(query)  # Execute the query
-    repoList = result['data']['viewer']['repositories']['edges']
+    result = resolve(viewer_query)  # Execute the query
+    viewerName = get_name_from_query(result)
+    repoList = result['viewer']['repositories']['edges']
     repoNames = extract_repos_from_list(repoList)
+    datesList = check_repo(repoNames, viewerName)
+    print(datesList)
     return "OK"
 
 
