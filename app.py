c689e6faa823eec5a1095b17c18481a66016c9e5
Ryan McKelvey <ryanmckelvey98@gmail.com>
2021-03-29 14:09:25 +0100

    Adding a function to extract list of repos from JSON
    Adding a function to extract list of repos from JSON for later use in queries

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 # A Flask app set up using Githubs GraphQL API
 import requests
+import sys
 from flask import Flask as fl
 
 app = fl(__name__)
 
-#github_auth = "Your Github PAT here"
+github_auth = "Your Github PAT here"
 
 headers = {"Authorization": github_auth}
 
+# Query to get a users list of repos
 query = """
 {
-  user(login: "YOUR LOGIN") {
+  user(login: "YOUR LOGIN HERE") {
     name
-    repositories(privacy: PUBLIC, first: 100, after: "CURSOR") {
+    repositories(privacy: PUBLIC, first: 100, after: "CURSOR OF BOUNDARY REPO") {
       edges {
         node {
           name
@@ -23,7 +25,9 @@ query = """
 }
 """
 
-##Function to execute GraphQL queries
+# Function to execute GraphQL queries
+
+
 def run_query(query):
     request = requests.post('https://api.github.com/graphql',
                             json={'query': query}, headers=headers)
@@ -33,8 +37,21 @@ def run_query(query):
         raise Exception("Query failed to run by returning code of {}. {}".format(
             request.status_code, query))
 
+##Function to get a string list of repository names for querying
+def extract_repos_from_list(Lst):
+    simplified_list = []
+    for node in Lst:
+        simplified_list.append(node['node']['name'])
+    return simplified_list
+
 
 @app.route('/')
 def get_repos():
     result = run_query(query)  # Execute the query
-    return result
+    repoList = result['data']['user']['repositories']['edges']
+    repoNames = extract_repos_from_list(repoList)
+    return "OK"
+
+
+if __name__ == "___main___":
+    app.run(debug=True)
